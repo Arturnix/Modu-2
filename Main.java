b04941d2c35a669b0ca1909a4b45951e50a0e052
Arturnix <artur.zgodka.programista@gmail.com>
2023-11-29 00:51:22 +0100

    Update gitignore

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,6 +11,26 @@ public class Main {
         //System.out.println(NajmniejszaLiczba.lowestValue());
         //NajmniejszaLiczba.lowestValueArray();
 
-        Trojkat.printStarTriangle();
+        //Trojkat.printStarTriangle();
+
+        //WydrukowanieLiczb.printRangeOfNumbers();
+
+        //SumowanieLiczb.sumNumbers();
+
+        //Fibonacci
+        /*int numberOfElements;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Provide number of elements > 0");
+            numberOfElements = scanner.nextInt();
+        } while(numberOfElements <= 0);
+        scanner.close();
+        CiagFibonacciego.fibonacciSequence(numberOfElements);*/
+
+        //Silnia.policzSilnie(4);
+
+        Calculator calc = new Calculator();
+        calc.start();
     }
 }
